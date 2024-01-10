91668a19a3123576ec5c4bd8f1577261d363aa95
Fanny Rizzi <fannyrizzi@MacBook-Air-de-Fanny-2.local>
2024-01-10 18:26:13 +0100

    brain done

---
+++ Module_01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 17:59:36 by inaranjo          #+#    #+#             */
+/*   Updated: 2024/01/10 18:06:30 by inaranjo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::string brain = "HI THIS IS BRAIN";
+
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << &brain << ": " << brain << std::endl;
+    std::cout << stringPTR << ": " << *stringPTR << std::endl;
+    std::cout << &stringREF << ": " << stringREF << std::endl;
+
+    return 0;
+}
