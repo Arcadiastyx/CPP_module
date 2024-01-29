161a4f9c481a1a181cb22d034b088590888acbcc
Fanny Rizzi <fannyrizzi@MacBook-Air-de-Fanny-2.local>
2024-01-29 11:05:18 +0100

    adding new features

--- Module_05/ex00/Bureaucrat.cpp
+++ Module_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:15:29 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/01/29 10:21:46 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/01/29 11:04:24 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ Bureaucrat::Bureaucrat()
     std::cout << "[Constructor Called]" << std::endl;
 }
 
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade)
+{
+    std::cout << "[Constructor Parameter Called]" << std::endl;
+    if(grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if(grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+
+}
 
 Bureaucrat::Bureaucrat(const Bureaucrat &cpy)
 {
@@ -36,8 +45,38 @@ Bureaucrat & Bureaucrat::operator=(const Bureaucrat &rhs)
 
     if(this != &rhs)
     {
-        
+        _grade = rhs._grade;
     }
     return *this;
 
+}
+
+std::string Bureaucrat::getName()const
+{
+    return (this->_name);
+}
+
+int     Bureaucrat::getGrade() const
+{
+    return (this->_grade);
+}
+
+void    Bureaucrat::incrementGrade() 
+{
+    if ( _grade - 1 < 1 )
+        throw Bureaucrat::GradeTooHighException();
+    _grade--;
+}
+
+void    Bureaucrat::decrementGrade()
+{
+    if ( _grade + 1 > 150 )
+        throw Bureaucrat::GradeTooLowException();
+    _grade++;
+}
+
+std::ostream& operator<<( std::ostream& o, const Bureaucrat& rhs )
+{
+    o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+    return o;
 }
--- Module_05/ex00/Bureaucrat.hpp
+++ Module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 10:12:28 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/01/29 10:16:30 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/01/29 10:57:04 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,37 @@
 
 class Bureaucrat
 {
-    private: 
+    private:
+        const std::string   _name; 
+        int                 _grade;
 
     public:
         Bureaucrat();
+        Bureaucrat(const std::string &name, int grade);
         Bureaucrat(const Bureaucrat &cpy);
         ~Bureaucrat();
         Bureaucrat &operator=(const Bureaucrat &rhs);
+
+        std::string getName() const;
+        int         getGrade() const;
+
+        void          incrementGrade();
+        void          decrementGrade();
+
+        /* ---------------- Exception Classes ---------------- */
+        class GradeTooHighException : public std::exception 
+        {
+            public:
+            virtual const char* what() const throw() { return "Grade too high"; }
+        };
+        
+        class GradeTooLowException : public std::exception 
+        {
+            public:
+            virtual const char* what() const throw() { return "Grade too low"; }
+        };
 };
 
-
+std::ostream&   operator<<( std::ostream& o, const Bureaucrat& rhs );
 
 #endif
