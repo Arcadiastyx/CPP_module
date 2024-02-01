463e0ddbbee08076b139f4315b93cac87909ab5e
Fanny Rizzi <fannyrizzi@MacBook-Air-de-Fanny-2.local>
2024-02-01 10:06:20 +0100

    adding comment + main

--- Module_06/ex02/main.cpp
+++ Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 17:28:05 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/01/31 17:28:10 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/01 10:02:51 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,27 @@
 #include <cstdlib>
 #include <ctime>
 
-class Base {
-public:
-    virtual ~Base() {}
+/*CLASS PRICIPALE*/
+class Base 
+{
+    public:
+        virtual ~Base() {}
 };
 
+/*CLASS HERITAGE DE LA PRICIPALE*/
 class A : public Base {};
 class B : public Base {};
 class C : public Base {};
 
-Base *generate(void) {
+/*
+    RETURN UNE CLASS HERITAGE DE FACON ALEATOIRE,
+    LA SEED TIME ET UTILISEE AFIN DE S ARRURER D AVOIR UN RETOUR RAND A CHAQUE LANCEMENT DU PROG
+*/
+Base *generate(void)
+{
     srand(time(nullptr));
-    switch (rand() % 3) {
+    switch (rand() % 3)
+    {
     case 0:
         return new A();
     case 1:
@@ -37,7 +46,9 @@ Base *generate(void) {
     }
 }
 
-void identify(Base* p) {
+/*PERMET L IDENTIFICATION D UN OBJ, COMME C EST UN POINTEUR D HERITAGE DYNAMIQUE CAST ET UTILISE*/
+void identify(Base* p)
+{
     if (dynamic_cast<A*>(p))
         std::cout << "A\n";
     else if (dynamic_cast<B*>(p))
@@ -46,7 +57,9 @@ void identify(Base* p) {
         std::cout << "C\n";
 }
 
-void identify(Base& p) {
+/*PERMET L IDENTIFICATION D UN OBJ, PTR VS REF / PTR = NULL / REF = NE PEUT PAS ETRE NULL*/
+void identify(Base& p)
+{
     if (dynamic_cast<A*>(&p))
         std::cout << "A\n";
     else if (dynamic_cast<B*>(&p))
@@ -55,7 +68,8 @@ void identify(Base& p) {
         std::cout << "C\n";
 }
 
-int main() {
+int main()
+{
     Base *ptr = generate();
     identify(ptr);
     identify(*ptr);
