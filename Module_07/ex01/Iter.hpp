200aa940f58a6d42a7de399db0924487d209b599
inaranjo <inaranjo@c1r9s8.42lausanne.ch>
2024-02-01 15:16:50 +0100

    adding colors

--- Module_07/ex01/Iter.hpp
+++ Module_07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:00:33 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/01 15:08:14 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/01 15:14:41 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 #define ITER_HPP
 
 #include <iostream>
+
+#define YELLOW "\e[93;5;226m"
+#define RESET  "\e[0m"
+#define GREEN  "\e[92;5;118m"
+#define VIOLET "\033[35m"
+#define RED "\033[31m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE "\033[34m"
+#define LIGHT_BLUE "\033[36m"
+#define LIGHT_CYAN "\033[96m"
+#define LIGHT_PURPLE "\033[95m"
+#define MAGENTA "\033[35m"
+#define LIGHT_ORANGE "\033[38;5;208m"
+#define GRAY "\033[90m"
 
 template <typename T, typename Function>
 void iter(T* array, size_t length, Function func) 
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 14:03:43 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/01 14:15:00 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/01 15:16:04 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 /*
     Il est egalement possible de mettre en temple non pas un type mais egalement des fonctions : 
-        - ainsi on peut faire de la programation generique
+        - ainsi on peut faire de la programation generique : 
+        - iter peut travailler avec nimporte quel type de tableau     
 */
 
 int main()
@@ -23,13 +24,13 @@ int main()
     double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::string stringArray[] = {"one", "two", "three", "four", "five"};
 
-    std::cout << "Integer array:" << std::endl;
+    std::cout << YELLOW << "[Integer array:]" << RESET << std::endl;
     iter(intArray, 5, printElement<int>);
 
-    std::cout << "Double array:" << std::endl;
+    std::cout << YELLOW << "[Double array:]" << RESET << std::endl;
     iter(doubleArray, 5, printElement<double>);
 
-    std::cout << "String array:" << std::endl;
+    std::cout << YELLOW << "[String array:]" << RESET <<std::endl;
     iter(stringArray, 5, printElement<std::string>);
 
     return 0;
