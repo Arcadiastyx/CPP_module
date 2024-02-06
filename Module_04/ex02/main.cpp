fbf72329da89541f0ed86ef7c035abb3b7334804
inaranjo <inaranjo@c1r9s8.42lausanne.ch>
2024-02-06 17:15:55 +0100

    adding main

--- Module_04/ex02/main.cpp
+++ Module_04/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
+/*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 12:44:07 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/01/25 19:58:05 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/06 17:13:10 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,39 @@
 #include "WrongCat.hpp"
 #include "Brain.hpp"
 
+
 int main( void )
 {
-    return 0;
+   std::cout << "---------------Animals out of Array---------------" << std::endl << std::endl;
+    
+    const AAnimal* i = new Cat();
+    const AAnimal* j = new Dog();
+    
+
+    std::cout <<std::endl;
+    std::cout << "---------------Animals in the Array---------------" << std::endl << std::endl;
+    
+    const AAnimal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
+    for ( int i = 0; i < 4; i++ ) {
+        delete animals[i];
+    }
+    
+
+    std::cout <<std::endl;
+    std::cout << "---------------Idea s from Dog and Cat---------------" << std::endl << std::endl;
+    i->printIdeas();
+    j->printIdeas();
+    
+    delete i;
+    delete j;
+
+      std::cout <<std::endl;
+    std::cout << "---------------Animal s making Sounds---------------" << std::endl << std::endl;
+    //Animal sound;
+    Dog    sound1;
+    Cat    sound2;
+
+    //sound.makeSound();
+    sound1.makeSound();
+    sound2.makeSound();
 }
