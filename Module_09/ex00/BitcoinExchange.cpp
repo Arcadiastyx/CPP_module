b5a4746657aaea63b68165f2f9bdc4b05a940751
inaranjo <inaranjo@c1r9s8.42lausanne.ch>
2024-02-12 18:46:02 +0100

    adding fonctions

--- Module_09/ex00/BitcoinExchange.cpp
+++ Module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 13:35:08 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/12 16:49:46 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/12 18:45:12 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ BitcoinExchange::BitcoinExchange(const std::string& filename)
         std::string::size_type idx_str= line.find(',');
         if (idx_str == std::string::npos)
         {
-            std::cerr << "Error: Invalid format in database => " << line << '\n';
+            std::cerr << RED <<"[Error: Invalid format in database] " << RESET << line << '\n';
             continue;
         }
         std::string date = line.substr(0, idx_str);
@@ -52,9 +52,158 @@ BitcoinExchange::BitcoinExchange(const std::string& filename)
         std::istringstream rate_stream(rate_str);
         if (!(rate_stream >> rate)) 
         {
-            std::cerr << "Error: Invalid rate format in database => " << rate_str << '\n';
+            std::cerr << RED <<"[Error: Invalid rate format in database] " << RESET << rate_str << '\n';
             continue;
         }
-        rates[date] = rate;
+        Brates[date] = rate;
     }
+}
+
+
+/*
+    but: retourner le taux de change pour une date donnée, 
+         ou le taux de change le plus proche inférieur
+         si la date exacte n'est pas disponible
+    
+    - Recherche pour une date qui correspond exactement
+    - Recherche pour une date sans taux de change exact, mais avec une date antérieure disponible
+    - Recherche pour une date antérieure à toutes les dates disponibles
+    - Recherche pour une date postérieure à toutes les dates disponibles
+
+------------------------------------------------------------------------------
+1. Si la map est vide, retourne immédiatement -1.0
+2. Trouve le premier élément qui n'est pas considéré comme inférieur à 'date' (via iterateur)
+3. Si lower_bound retourne end(), toutes les dates dans la map sont inférieures à 'date'.
+    On retourne donc le taux de la dernière date disponible.
+4. Si la date exacte est trouvée, retourne son taux de change.
+5. Si l'itérateur est au début et que la date est inférieure à la première date de la map,
+    cela signifie qu'il n'y a pas de taux de change disponible pour cette date ou une date antérieure.
+6. Si l'itérateur est au début et que la date est inférieure à la première date de la map,
+    cela signifie qu'il n'y a pas de taux de change disponible pour cette date ou une date antérieure.
+7. Si la date exacte n'est pas trouvée et que nous ne sommes pas au début,
+    cela signifie que 'it' pointe sur le premier élément supérieur à 'date'.
+    Nous retournons donc le taux de change de l'élément juste avant 'it',
+    qui représente la date la plus proche inférieure à celle demandée
+-------------------------------------------------------------------------------
+Il est important de noter que MAP travaille en paire a savoir la cle + la value 
+c est pour cela qu on peut utiliser first pour la cle et second pour la value 
+     
+*/
+double BitcoinExchange::GetExchangeRate(const std::string& date)
+{
+    if (Brates.empty())
+        return -1.0;
+
+    std::map<std::string,double>::iterator it = Brates.lower_bound(date);
+
+    if (it == Brates.end())
+       return std::prev(it)->second;
+
+    if (it->first == date) 
+        return it->second;
+        
+    else if (it == Brates.begin())
+        return -1.0;
+        
+    else 
+        return std::prev(it)->second;
+}
+
+
+/*
+    1.Vérifier la longueur de la chaîne pour le format AAAA-MM-JJ
+    2.Vérifier les séparateurs
+    3.Extrait l annee, le mois, les jouer (AAAA MM JJ)
+    4.Vérifier les plages de l'année, du mois et du jour
+    5.Jours maximums par mois (février est traité séparément pour les années bissextiles)
+    6.Ajuster pour les années bissextiles, Février a 29 jours dans une année bissextile
+    7.Vérifier si le jour est valide pour le mois, comme on commence par 0 alors fevrier = 1;
+    8.Si toutes les vérifications sont passées
+*/
+bool BitcoinExchange::CheckDate(const std::string& date)
+{
+    if (date.length() != 10) return false;
+
+    if (date[4] != '-' || date[7] != '-') return false;
+
+    int year, month, day;
+    try
+    {
+        year = std::stoi(date.substr(0, 4));
+        month = std::stoi(date.substr(5, 2));
+        day = std::stoi(date.substr(8, 2));
+    } catch (...) 
+    {
+        return false;
+    }
+
+    if (year < 0 || month < 1 || month > 12 || day < 1) return false;
+
+    int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        daysInMonth[1] = 29;
+
+    if (day > daysInMonth[month - 1]) return false;
+
+    return true;
+}
+
+/*cette fonction verifie que non value ne sorte pas de leur plage de chiffre souhaite*/    
+bool BitcoinExchange::CheckValue(const double& value) 
+{
+	if (value < 0 || value > 1000)
+    {
+		std::cerr << "Error: Value "<< value<< " is not a positive number between 0 and 1000.\n";
+		return false;
+	}
+	return true;
+}
+
+void BitcoinExchange::ReadInput(const std::string &filename)
+{
+	std::ifstream file(filename.c_str());
+	std::string line;
+	std::getline(file, line); // Skip header line
+	
+    while (std::getline(file, line))
+    {
+		std::istringstream ss(line);
+		std::string date;
+		double value;
+		char delimiter;
+
+		if (!(ss >> date >> delimiter >> value))
+        {
+			std::cerr << "Error: Unable to parse line => " << line << '\n';
+			continue;
+		}
+
+		if (delimiter != '|')
+        {
+			std::cerr << "Error: Expected '|' delimiter in line => " << line << '\n';
+			continue;
+		}
+
+		if (!CheckDate(date)) 
+        {
+			std::cerr << "Error: Invalid date format in line => " << line << '\n';
+			continue;
+		}
+
+		if (!CheckValue(value)) 
+        {
+			continue;
+		}
+
+		double exchangeRate = GetExchangeRate(date);
+		if (exchangeRate == -1.0) 
+        {
+			std::cerr << "Error: No exchange rate available for date => " << date << '\n';
+			continue;
+		}
+
+		std::cout << date << " => "
+			  << value << " = " << value * exchangeRate
+			  << '\n';
+	}
 }
--- Module_09/ex00/BitcoinExchange.hpp
+++ Module_09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 13:35:06 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/12 13:57:55 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/12 18:42:58 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,28 @@
 #include <string>
 #include <map>
 
+#define YELLOW "\e[93;5;226m"
+#define RESET  "\e[0m"
+#define GREEN  "\e[92;5;118m"
+#define VIOLET "\033[35m"
+#define RED "\033[31m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE "\033[34m"
+#define LIGHT_BLUE "\033[36m"
+#define LIGHT_CYAN "\033[96m"
+#define LIGHT_PURPLE "\033[95m"
+#define MAGENTA "\033[35m"
+#define LIGHT_ORANGE "\033[38;5;208m"
+#define GRAY "\033[90m"
+
 class BitcoinExchange
 {
     private:
-        std::map<std::string, double> rates; 
+        std::map<std::string, double> Brates;
+        double GetExchangeRate(const std::string& date);
+        bool   CheckDate(const std::string& date);
+        bool   CheckValue(const double& value);
+        void   ReadInput(const std::string &filename);
 
     public:
         BitcoinExchange();
