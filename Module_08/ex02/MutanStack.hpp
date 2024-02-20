5376812b5cc0285e74c92c6d4f05ab33397ee443
inaranjo <inaranjo@c1r9s8.42lausanne.ch>
2024-02-20 16:30:26 +0100

    adding tester for main

--- Module_08/ex02/MutanStack.hpp
+++ Module_08/ex02/MutanStack.hpp
@@ -3,21 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   MutanStack.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
+/*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:52:50 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/08 14:58:40 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/20 15:49:32 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
-#define 
+#ifndef MUTANSTACK_HPP
+#define MUTANSTACK_HPP
 
 #include <iostream>
 #include <algorithm>
 #include <list>
 #include <stack>
 #include <deque>
+
+#define YELLOW "\e[93;5;226m"
+#define RESET  "\e[0m"
+#define GREEN  "\e[92;5;118m"
+#define VIOLET "\033[35m"
+#define RED "\033[31m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE "\033[34m"
+#define LIGHT_BLUE "\033[36m"
+#define LIGHT_CYAN "\033[96m"
+#define LIGHT_PURPLE "\033[95m"
+#define MAGENTA "\033[35m"
+#define LIGHT_ORANGE "\033[38;5;208m"
+#define GRAY "\033[90m"
+
 
 template< typename T, class Container = std::deque< T > > 
 class MutantStack : public std::stack< T, Container >
@@ -31,8 +46,8 @@ class MutantStack : public std::stack< T, Container >
         MutantStack( const MutantStack& rhs ) { *this = rhs; }
         MutantStack&    operator=( const MutantStack& rhs )
         {
-        std::stack< T, Container >::operator=( rhs );
-        return *this;
+            std::stack< T, Container >::operator=( rhs );
+            return *this;
         }
 
     typedef typename Container::iterator    iterator;
--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -3,10 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inaranjo <inaranjo <inaranjo@student.42    +#+  +:+       +#+        */
+/*   By: inaranjo <inaranjo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:52:47 by inaranjo          #+#    #+#             */
-/*   Updated: 2024/02/08 14:52:48 by inaranjo         ###   ########.fr       */
+/*   Updated: 2024/02/20 16:29:10 by inaranjo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutanStack.hpp"
+
+int main()
+{
+    std::cout << "/ ***** MutantStack ***** /" << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << YELLOW << "[this is the top of the stack]> "  << RESET << mstack.top() << std::endl;
+
+    mstack.pop();
+    std::cout << VIOLET << "[The current size of the stack]> " << RESET << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::cout << ORANGE <<"[Copy instance creation for a stack name s]" << RESET << std::endl;
+    std::stack<int> s(mstack);
+
+    std::cout << "Stack content from 's' : ";
+    while (!s.empty())
+    {
+        std::cout << s.top() << " ";
+        s.pop();
+    }
+    std::cout << std::endl;
+
+    /* -------------------------------------------------------------------- */
+
+    std::cout << std::endl;
+    std::cout << "/ ***** List ***** /" << std::endl;
+
+    std::list< int > lst;
+
+    lst.push_back(5);
+    lst.push_back(17);
+
+    std::cout << lst.back() << std::endl;
+
+    lst.pop_back();
+    std::cout << lst.size() << std::endl;
+
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    //[...]
+    lst.push_back(0);
+
+    std::list<int>::iterator lst_it = lst.begin();
+    std::list<int>::iterator lst_ite = lst.end();
+    ++it;
+    --it;
+    while (lst_it != lst_ite)
+    {
+        std::cout << *lst_it << std::endl;
+        ++lst_it;
+    }
+    std::list<int> l(lst);
+
+    return 0;
+}
